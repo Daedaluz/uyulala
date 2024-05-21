be5afbe51a8a4fce449e4fef73a143c624d8c101
Tobias Assarsson <tobias.assarsson@gmail.com>
2024-05-21 09:35:20 +0200

    more bgs

--- frontend/src/Layout/RootLayout.tsx
+++ frontend/src/Layout/RootLayout.tsx
@@ -2,13 +2,20 @@ import './Layout.css';
 import {Outlet} from 'react-router-dom';
 import {Alerts} from "../Context/Alert.tsx";
 import {useTheme} from "@mui/material";
+import {useMemo} from "react";
 
 export const RootLayout = () => {
     const theme = useTheme()
+    const bg = useMemo(()=> {
+        const bgArray = ["1.jpg", "2.jpg"];
+        const index = Math.floor(Math.random() * bgArray.length);
+        return `url('/bg/${bgArray[index]}')`;
+    }, []);
+
     return (
         <>
             <Alerts/>
-            <div className={'layout'} style={{backgroundColor: theme.palette.background.default}}>
+            <div className={'layout'} style={{backgroundColor: theme.palette.background.default, backgroundImage: bg}}>
                 <Outlet/>
             </div>
         </>
